f621fc6d8b18e1576c1b4c06aa7c23a1cd793de1
Zedroid <danielboss539@gmail.com>
2025-03-31 01:53:28 -0400

    trust

--- src/main/java/net/zedroid/testgui/TestguiDataGenerator.java
+++ src/main/java/net/zedroid/testgui/TestguiDataGenerator.java
@@ -2,7 +2,7 @@ package net.zedroid.testgui;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-
+	// tres importante commente
 public class TestguiDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
