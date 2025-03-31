10b07a6d75712e6280aad9f9a8eabb4f0a578ab6
Zedroid <danielboss539@gmail.com>
2025-03-31 01:42:41 -0400

    idk it didnt work first time

---
+++ src/main/java/net/zedroid/testgui/TestGUIClient.java
@@ -0,0 +1,10 @@
+package net.zedroid.testgui;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestGUIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/zedroid/testgui/TestguiDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zedroid.testgui;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestguiDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
